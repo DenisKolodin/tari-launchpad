9ca90b9f010e17ce52e9889473ee948b268de7b6
Denis Kolodin <deniskolodin@gmail.com>
2023-04-13 16:37:48 +0200

    feat: add core crates

---
+++ core/protocol/src/app.rs
@@ -0,0 +1,10 @@
+use crate::job::{JobId, JobState};
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+
+#[derive(Default, Debug, Clone, Serialize, Deserialize)]
+pub struct AppState {
+    pub jobs: HashMap<JobId, JobState>,
+}
+
+pub enum AppDelta {}
