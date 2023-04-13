25e02f5fc3be8464dde57833c12911431f48f866
Denis Kolodin <deniskolodin@gmail.com>
2023-04-13 17:13:09 +0200

    feat: return errors from actors' handlers

--- core/tact/src/actor/address.rs
+++ core/tact/src/actor/address.rs
@@ -1,7 +1,23 @@
-use super::handler::{Actor, Envelope};
+use super::handler::{Actor, Envelope, OnEvent};
+use thiserror::Error;
 use tokio::sync::mpsc;
+
+#[derive(Debug, Error)]
+#[error("Can't send an event to an actor")]
+pub struct SendError;
 
 #[derive(Debug, Clone)]
 pub struct Address<A: Actor> {
     tx: mpsc::UnboundedSender<Envelope<A>>,
+}
+
+impl<A: Actor> Address<A> {
+    pub fn send<E>(&self, event: E) -> Result<(), SendError>
+    where
+        A: OnEvent<E>,
+        E: Send + 'static,
+    {
+        let envelope = Envelope::from_event(event);
+        self.tx.send(envelope).map_err(|_| SendError)
+    }
 }
--- core/tact/src/actor/handler.rs
+++ core/tact/src/actor/handler.rs
@@ -6,25 +6,43 @@ pub trait Actor: Send + Sized {}
 
 #[async_trait]
 pub trait OnEvent<E>: Actor {
-    async fn handle(&mut self, event: E, ctx: &mut ActorContext<Self>);
+    async fn handle(&mut self, event: E, ctx: &mut ActorContext<Self>) -> Result<(), Error>;
 }
 
 pub struct Envelope<A: Actor> {
     handler: Box<dyn Handler<A>>,
 }
 
-#[async_trait]
-pub trait Handler<A: Actor>: Send {
-    async fn handle(self: Box<Self>, actor: &mut A, ctx: &mut ActorContext<A>);
+impl<A: Actor> Envelope<A> {
+    pub fn from_event<E>(event: E) -> Self
+    where
+        A: OnEvent<E>,
+        E: Send + 'static,
+    {
+        let handler = HandlerImpl { event };
+        Self {
+            handler: Box::new(handler),
+        }
+    }
 }
 
-pub struct HandlerImpl<E> {
+#[async_trait]
+trait Handler<A: Actor>: Send {
+    async fn handle(self: Box<Self>, actor: &mut A, ctx: &mut ActorContext<A>)
+        -> Result<(), Error>;
+}
+
+struct HandlerImpl<E> {
     event: E,
 }
 
 #[async_trait]
 impl<A: OnEvent<E>, E: Send> Handler<A> for HandlerImpl<E> {
-    async fn handle(self: Box<Self>, actor: &mut A, ctx: &mut ActorContext<A>) {
+    async fn handle(
+        self: Box<Self>,
+        actor: &mut A,
+        ctx: &mut ActorContext<A>,
+    ) -> Result<(), Error> {
         actor.handle(self.event, ctx).await
     }
 }
