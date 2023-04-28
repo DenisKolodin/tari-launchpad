b9eba8a279302bfc2803e17df4da82fc2354cf14
Denis Kolodin <deniskolodin@gmail.com>
2023-04-28 18:10:46 +0200

    feat: add the NormalScene component

--- cli/src/component/normal/mod.rs
+++ cli/src/component/normal/mod.rs
@@ -1,4 +1,12 @@
+mod mining;
+
+use crate::component::elements::block_with_title;
+use crate::component::{Component, Focus, Frame, Input};
+use crate::state::LaunchpadState;
+use crossterm::event::KeyEvent;
 use strum::{Display, EnumCount, EnumIter, FromRepr};
+use tui::backend::Backend;
+use tui::layout::{Constraint, Direction, Layout, Rect};
 
 #[derive(Debug, EnumCount, EnumIter, FromRepr, Clone, Copy, Display)]
 pub enum NormalTabs {
@@ -6,4 +14,32 @@ pub enum NormalTabs {
     #[strum(serialize = "Base Node")]
     BaseNode,
     Wallet,
+}
+
+pub struct NormalScene {}
+
+impl NormalScene {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Input for NormalScene {
+    fn on_input(&mut self, key: KeyEvent) -> Option<Focus> {
+        None
+    }
+}
+
+impl<B: Backend> Component<B> for NormalScene {
+    type State = LaunchpadState;
+
+    fn draw(&self, f: &mut Frame<B>, rect: Rect, state: &Self::State) {
+        let constraints = [Constraint::Length(1), Constraint::Min(0)];
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(constraints)
+            .split(rect);
+        let block = block_with_title(None);
+        f.render_widget(block, rect);
+    }
 }
