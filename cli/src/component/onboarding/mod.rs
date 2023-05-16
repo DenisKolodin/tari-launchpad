d4e99766949e404ee73ab990bbbd8a53228672e6
Denis Kolodin <deniskolodin@gmail.com>
2023-05-16 12:50:07 +0200

    feat: add the bot image

--- cli/src/component/onboarding/mod.rs
+++ cli/src/component/onboarding/mod.rs
@@ -9,13 +9,10 @@ use message::MessageWidget;
 use strum::{Display, EnumCount, EnumIter, FromRepr};
 use tui::backend::Backend;
 use tui::layout::{Constraint, Direction, Layout, Rect};
-use tui::style::Color;
+use tui::style::{Color, Style};
+use tui::text::{Span, Spans};
+use tui::widgets::Paragraph;
 
-const BOT: &str = "
-╓   ╖
-║O O║
-╙   ╜
-";
 const MSG_1: &str = "
 Hi! My name is T-Bot. It is a great pleasure and an honor to meet you!
 I have no memory of human faces, so if our paths have already crossed in the Aurora app, I’m glad to see you again!
@@ -52,16 +49,39 @@ impl<B: Backend> Component<B> for OnboardingScene {
             .constraints(constraints)
             .split(rect);
         let constraints = [
-            Constraint::Percentage(20),
-            Constraint::Percentage(60),
-            Constraint::Percentage(20),
+            Constraint::Percentage(30),
+            Constraint::Percentage(40),
+            Constraint::Percentage(30),
         ];
         let v_chunks = Layout::default()
             .direction(Direction::Vertical)
             .constraints(constraints)
             .split(h_chunks[1]);
+
+        let constraints = [Constraint::Min(0), Constraint::Length(3)];
+        let view_chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .constraints(constraints)
+            .split(v_chunks[1]);
+
         if let Some(message) = self.messages.last() {
-            message.draw(f, v_chunks[1], state);
+            message.draw(f, view_chunks[0], state);
         }
+
+        let constraints = [Constraint::Min(0), Constraint::Length(5)];
+        let line_chinks = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(constraints)
+            .split(view_chunks[1]);
+
+        let style = Style::default().fg(Color::White); //.bg(Color::Magenta);
+        let text = vec![
+            // Spans::from(Span::styled("╓   ╖", style)),
+            Spans::from(Span::styled("[o o]", style)),
+            // Spans::from(Span::styled("[- -]", style)),
+            // Spans::from(Span::styled("╙   ╜", style)),
+        ];
+        let bot = Paragraph::new(text);
+        f.render_widget(bot, line_chinks[1]);
     }
 }
