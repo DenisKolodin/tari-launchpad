122f9bdcab17bcfa25c6717c4f358d68ec9d5e3f
Denis Kolodin <deniskolodin@gmail.com>
2023-05-17 14:38:45 +0200

    feat: add the actions handler

--- cli/src/onboarding.rs
+++ cli/src/onboarding.rs
@@ -1,7 +1,8 @@
 use crate::state::bus::Bus;
+use crate::state::launchpad::LaunchpadAction;
 use anyhow::Error;
 use async_trait::async_trait;
-use tact::actors::{Actor, ActorContext};
+use tact::actors::{Actor, ActorContext, Do};
 
 pub struct OnboardingWorker {
     bus: Bus,
@@ -9,7 +10,19 @@ pub struct OnboardingWorker {
 
 #[async_trait]
 impl Actor for OnboardingWorker {
-    async fn initialize(&mut self, _ctx: &mut ActorContext<Self>) -> Result<(), Error> {
+    async fn initialize(&mut self, ctx: &mut ActorContext<Self>) -> Result<(), Error> {
+        self.bus.subscribe(ctx.recipient());
+        Ok(())
+    }
+}
+
+#[async_trait]
+impl Do<LaunchpadAction> for OnboardingWorker {
+    async fn handle(
+        &mut self,
+        event: LaunchpadAction,
+        ctx: &mut ActorContext<Self>,
+    ) -> Result<(), Error> {
         Ok(())
     }
 }
