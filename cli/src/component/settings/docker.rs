76c2a2c43018dba77ef67ef0b326241a1d66b160
Denis Kolodin <deniskolodin@gmail.com>
2023-05-29 18:10:23 +0200

    feat: add fields for the docker

--- cli/src/component/settings/docker.rs
+++ cli/src/component/settings/docker.rs
@@ -1,15 +1,23 @@
 use crate::component::elements::block_with_title;
+use crate::component::labeled_input::LabeledInput;
+use crate::component::widgets::separator::Separator;
 use crate::component::{Component, ComponentEvent, Frame, Input};
 use crate::state::AppState;
 
 use tui::backend::Backend;
-use tui::layout::Rect;
+use tui::layout::{Constraint, Direction, Layout, Rect};
 
-pub struct DockerSettings {}
+pub struct DockerSettings {
+    docker_tag: LabeledInput,
+    docker_registry: LabeledInput,
+}
 
 impl DockerSettings {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            docker_tag: LabeledInput::new("Docker Tag"),
+            docker_registry: LabeledInput::new("Docker Registry"),
+        }
     }
 }
 
@@ -20,8 +28,31 @@ impl Input for DockerSettings {
 impl<B: Backend> Component<B> for DockerSettings {
     type State = AppState;
 
-    fn draw(&self, f: &mut Frame<B>, rect: Rect, _state: &Self::State) {
+    fn draw(&self, f: &mut Frame<B>, rect: Rect, state: &Self::State) {
         let block = block_with_title(Some("Docker Settings"), false);
+        let inner_rect = block.inner(rect);
         f.render_widget(block, rect);
+        let constraints = [
+            // Expert
+            Constraint::Length(1),
+            Constraint::Length(3),
+            Constraint::Length(3),
+            // Image Statuses
+            Constraint::Length(1),
+            Constraint::Min(0),
+        ];
+        let chunks = Layout::default()
+            .vertical_margin(1)
+            .horizontal_margin(3)
+            .direction(Direction::Vertical)
+            .constraints(constraints)
+            .split(inner_rect);
+        let sep = Separator::new("Expert");
+        f.render_widget(sep, chunks[0]);
+        self.docker_tag.draw(f, chunks[1], state);
+        self.docker_registry.draw(f, chunks[2], state);
+
+        let sep = Separator::new("Expert");
+        f.render_widget(sep, chunks[3]);
     }
 }
