2000aa449ff78187522454941e2a8a830bdf9780
Denis Kolodin <deniskolodin@gmail.com>
2023-07-23 16:54:24 +0200

    fix: remove unused code

--- core/engine/src/container/mod.rs
+++ core/engine/src/container/mod.rs
@@ -132,8 +132,6 @@ pub struct ContainerTask {
     docker: Docker,
     mc: Box<dyn ManagedContainer>,
     container_info: ContainerInfo,
-    // TODO: Remove this state
-    state: ContainerState,
     pull_progress: u8,
     events: Option<Receiver>,
     status: Status,
@@ -158,7 +156,6 @@ impl ContainerTask {
             docker,
             mc: Box::new(mc),
             container_info,
-            state: ContainerState::Idle,
             pull_progress: 0,
             events: None,
             status: Status::InitialState,
@@ -274,59 +271,6 @@ impl Do<ProcessChanges> for ContainerTask {
     }
 }
 
-struct CheckImage;
-
-#[async_trait]
-impl Do<CheckImage> for ContainerTask {
-    type Error = Error;
-
-    async fn handle(
-        &mut self,
-        _: CheckImage,
-        ctx: &mut ActorContext<Self>,
-    ) -> Result<(), Self::Error> {
-        log::info!("Checking the image: {}", self.image());
-        let exist = self
-            .docker
-            .inspect_image(&self.container_info.image_name)
-            .await
-            .is_ok();
-        if !exist {
-            ctx.do_next(PullImage)?;
-        } else {
-            log::info!("The image exists: {}", self.image());
-        }
-        Ok(())
-    }
-}
-
-struct PullImage;
-
-#[async_trait]
-impl Do<PullImage> for ContainerTask {
-    type Error = Error;
-
-    async fn handle(
-        &mut self,
-        _: PullImage,
-        ctx: &mut ActorContext<Self>,
-    ) -> Result<(), Self::Error> {
-        let from_image = self.image().to_string();
-        log::info!("Pulling the image: {from_image}");
-        let opts = Some(CreateImageOptions {
-            from_image,
-            ..Default::default()
-        });
-        let stream = self
-            .docker
-            .create_image(opts, None, None)
-            .map(PullProgress::from);
-        let receiver = Receiver::connect(stream, ctx.recipient());
-        self.state = ContainerState::PullingImage(receiver);
-        Ok(())
-    }
-}
-
 #[derive(Debug, Error)]
 enum PullError {
     #[error("Docker error: {0}")]
@@ -375,152 +319,6 @@ impl Do<PullProgress> for ContainerTask {
     ) -> Result<(), Error> {
         // TODO: Handle pull errors
         // Restart pulling, etc...
-        Ok(())
-    }
-}
-
-#[derive(Debug, Clone, From)]
-struct CreateContainer;
-
-#[async_trait]
-impl Do<CreateContainer> for ContainerTask {
-    type Error = Error;
-
-    async fn handle(
-        &mut self,
-        msg: CreateContainer,
-        ctx: &mut ActorContext<Self>,
-    ) -> Result<(), Self::Error> {
-        let mut args = Args::default();
-        self.mc.args(&mut args);
-        let mut ports = Ports::default();
-        self.mc.ports(&mut ports);
-        let mut envs = Envs::default();
-        self.mc.envs(&mut envs);
-        /*
-        let opts = CreateContainerOptions {
-            name: self.inner.container_name.clone(),
-            platform: None,
-        };
-
-        let mut networks = Networks::default();
-        self.inner.image.networks(&mut networks);
-        let networks = self.networks_map(networks)?;
-
-        let mut volumes = Volumes::default();
-        self.inner.image.volumes(&mut volumes);
-        let volumes = volumes_map(volumes.build());
-
-        let mut mounts = Mounts::default();
-        self.inner.image.mounts(&mut mounts);
-        let mounts = self.mounts_map(mounts.build())?;
-        let ports = ports.build();
-        let config = Config {
-            image: Some(self.inner.image_name.clone()),
-            attach_stdin: Some(false),
-            attach_stdout: Some(false),
-            attach_stderr: Some(false),
-            exposed_ports: Some(exposed_ports(&ports)),
-            open_stdin: Some(true),
-            stdin_once: Some(false),
-            tty: Some(true),
-            env: Some(envs.build()),
-            volumes: Some(volumes),
-            cmd: Some(args.build()),
-            host_config: Some(HostConfig {
-                binds: Some(vec![]),
-                network_mode: Some("bridge".to_string()),
-                port_bindings: Some(ports_map(&ports)),
-                mounts: Some(mounts),
-                ..Default::default()
-            }),
-            networking_config: Some(networks),
-            ..Default::default()
-        };
-        self.docker.create_container(Some(opts), config).await?;
-        */
-        Ok(())
-    }
-}
-
-#[derive(Debug, Clone, From)]
-struct StartContainer;
-
-#[async_trait]
-impl Do<StartContainer> for ContainerTask {
-    type Error = Error;
-
-    async fn handle(
-        &mut self,
-        msg: StartContainer,
-        ctx: &mut ActorContext<Self>,
-    ) -> Result<(), Self::Error> {
-        let name = &self.container_info.container_name;
-        self.docker.start_container::<String>(name, None).await?;
-        Ok(())
-    }
-
-    async fn fallback(&mut self, err: Error, ctx: &mut ActorContext<Self>) -> Result<(), Error> {
-        let name = &self.container_info.container_name;
-        log::error!("Can't start the container {name}: err");
-        let duration = Duration::from_secs(5);
-        let notifier = ctx.notifier(StartContainer);
-        let timeout = Timeout::spawn(duration, notifier);
-        self.state = ContainerState::WaitContainerStart(timeout);
-        Ok(())
-    }
-}
-
-#[derive(Debug, From)]
-struct KillContainer;
-
-#[async_trait]
-impl Do<KillContainer> for ContainerTask {
-    type Error = Error;
-
-    async fn handle(
-        &mut self,
-        msg: KillContainer,
-        ctx: &mut ActorContext<Self>,
-    ) -> Result<(), Self::Error> {
-        let name = &self.container_info.container_name;
-        self.docker.kill_container::<String>(name, None).await?;
-        Ok(())
-    }
-
-    async fn fallback(&mut self, err: Error, ctx: &mut ActorContext<Self>) -> Result<(), Error> {
-        let name = &self.container_info.container_name;
-        log::error!("Can't stop the container {name}: err");
-        // TODO: Recover after the error
-        Ok(())
-    }
-}
-
-#[derive(Debug, From)]
-struct RemoveContainer;
-
-#[async_trait]
-impl Do<RemoveContainer> for ContainerTask {
-    type Error = Error;
-
-    async fn handle(
-        &mut self,
-        msg: RemoveContainer,
-        ctx: &mut ActorContext<Self>,
-    ) -> Result<(), Self::Error> {
-        let opts = RemoveContainerOptions {
-            force: true,
-            ..Default::default()
-        };
-        let name = &self.container_info.container_name;
-        self.docker.remove_container(name, Some(opts)).await?;
-        Ok(())
-    }
-
-    async fn fallback(&mut self, err: Error, ctx: &mut ActorContext<Self>) -> Result<(), Error> {
-        let name = &self.container_info.container_name;
-        log::error!("Can't remove the container {name}: err");
-        // TODO: Recover after the error
         Ok(())
     }
 }
